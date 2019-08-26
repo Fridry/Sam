602575c8968c73835a661c18937047a3fa6ebfbc
Fridry <fredwin.ed@gmail.com>
2019-08-26 19:22:27 -0300

    Alterações

--- src/java/Models/TesteDAO.java
+++ src/java/Models/TesteDAO.java
@@ -5,10 +5,87 @@
  */
 package Models;
 
+import Util.HibernateUtil;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.PersistenceException;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author fridr
  */
-public class TesteDAO {
+public class TesteDAO implements Serializable {
+    private Session sessao;
+    private Transaction trans;
+    private List<Teste> list;
     
+    public void createTeste(Teste teste) {
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            trans = sessao.beginTransaction();
+            sessao.save(teste);
+            trans.commit();
+        } catch (PersistenceException e) {
+            trans.rollback();
+        } finally {
+            sessao.close();
+        }
+    }
+    
+    public void deleteTeste(Teste teste) {
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            trans = sessao.beginTransaction();
+            sessao.delete(teste);
+            trans.commit();
+        } catch (PersistenceException e) {
+            trans.rollback();
+        } finally {
+            sessao.close();
+        }
+    }
+    
+    public void updateTeste(Teste teste) {
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            trans = sessao.beginTransaction();
+            sessao.update(teste);
+            trans.commit();
+        } catch (PersistenceException e) {
+            trans.rollback();
+        } finally {
+            sessao.close();
+        }
+    }
+    
+    public List<Teste> getListTeste() {
+        try{
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        trans = sessao.beginTransaction();
+        Criteria cri = sessao.createCriteria(Teste.class);
+        list = cri.list();
+        }catch(RuntimeException e){
+            e.printStackTrace();
+        }finally{
+            sessao.close();
+        }
+        return list;
+    }
+    
+        public Teste getById(int id){
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            trans = sessao.beginTransaction();
+            Teste teste = new Teste();
+            return teste = (Teste) sessao.get(Teste.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            sessao.close();
+        }
+        return null;
+    }
 }
